dc103f71df76cf1046245acdfeb543e78ea53eb5
mycodingan <120147794+mycodingan@users.noreply.github.com>
2023-07-20 07:57:41 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -64,6 +64,8 @@ if (isset($_POST['simpan'])) { //untuk create
             $q1     = mysqli_query($koneksi, $sql1);
             if ($q1) {
                 $sukses     = "Berhasil memasukkan data baru";
+                header("refresh:2;url=data.php");
+                exit;
             } else {
                 $error      = "Gagal memasukkan data";
             }
@@ -159,61 +161,11 @@ if (isset($_POST['simpan'])) { //untuk create
                     </div>
                     <div class="col-12">
                         <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+                        <a href="data.php" class="btn btn-danger">cek data</a>
                     </div>
                 </form>
             </div>
         </div>
-
-        <!-- untuk mengeluarkan data -->
-        <div class="card">
-            <div class="card-header text-white bg-secondary">
-                Data siswa
-            </div>
-            <div class="card-body" >
-                <table class="table">
-                    <thead>     
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">NIM</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Alamat</th>
-                            <th scope="col">sekolah</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $sql2   = "select * from mahasiswa order by id desc";
-                        $q2     = mysqli_query($koneksi, $sql2);
-                        $urut   = 1;
-                        while ($r2 = mysqli_fetch_array($q2)) {
-                            $id         = $r2['id'];
-                            $nim        = $r2['nim'];
-                            $nama       = $r2['nama'];
-                            $alamat     = $r2['alamat'];
-                            $sekolah   = $r2['sekolah'];
-
-                        ?>
-                            <tr>
-                                <th scope="row"><?php echo $urut++ ?></th>
-                                <td scope="row"><?php echo $nim ?></td>
-                                <td scope="row"><?php echo $nama ?></td>
-                                <td scope="row"><?php echo $alamat ?></td>
-                                <td scope="row"><?php echo $sekolah ?></td>
-                                <td scope="row">
-                                    <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-warning">Edit</button></a>
-                                    <a href="index.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger">Delete</button></a>            
-                                </td>
-                            </tr>
-                        <?php
-                        }
-                        ?>
-                    </tbody>
-                    
-                </table>
-            </div>
-        </div>
-    </div>
 </body>
 
 </html>
